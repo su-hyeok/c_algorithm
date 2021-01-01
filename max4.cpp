4955a380c6af1edd1db2da459fb46244c99d9dfa
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-01-01 16:17:17 +0900

    Q1
    max4 로만 표현하지 않기 주의

---
+++ max4.cpp
@@ -0,0 +1,29 @@
+/*Q1.네 값의 최댓값을 구하는 함수 max4를 작성하세요*/
+#include <stdio.h>
+
+int max4(int a, int b, int c, int d)
+{
+	int max = a;
+	if (max < b) max = b;
+	if (max < c) max = c;
+	if (max < d) max = d;
+
+	return max;
+
+}
+
+int main(void)
+{
+	int a, b, c, d;
+
+	printf("네 정수의 최댓값을 구합니다\\n");
+	printf("a의 값: "); scanf_s("%d", &a);
+	printf("b의 값: "); scanf_s("%d", &b);
+	printf("c의 값: "); scanf_s("%d", &c);
+	printf("d의 값: "); scanf_s("%d", &d);
+
+	printf("최댓값은 %d입니다\\n", max4(a, b, c, d));
+
+	return 0;
+
+}
