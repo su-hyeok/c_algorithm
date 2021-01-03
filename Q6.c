cef80726060ee21c8946e5c2260f13b335ff1ba4
su-hyeok <64292838+su-hyeok@users.noreply.github.com>
2021-01-03 23:41:30 +0900

    modify

--- Q6.c
+++ Q6.c
@@ -1,6 +1,6 @@
 //while문이 종료될 때 변수 i의 값이 n+1이 됨을 확인하세요
 #include<stdio.h>
-/*int main(){
+int main(){
     int i, n;
     int sum;
     puts("1부터 n까지의 합을 구합니다.");
@@ -8,18 +8,10 @@
     scanf("%d",&n);
     sum  = 0;
     i = 1;
-
     while (i<=n){
      sum +=i;
      i++;
     }
     printf("1부터 %d까지의 합은 %d입니다.\n",n,sum);
     printf("i의 값은 %d입니다.\n",i);
-}*/
-int main(){
-    int n;
-    printf("숫자를 입력하세요: ");
-    scanf("%d",&n);
-    printf("값은 %d 입니다.\n",n+1);
-
 }
