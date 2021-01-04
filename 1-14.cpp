f6785e62da95236a93d2af198c345b72cbfdcb38
zsueue <zsueue@gmail.com>
2021-01-04 22:34:03 +0900

    Create 1-14.cpp

---
+++ 1-14.cpp
@@ -0,0 +1,19 @@
+///*14. 입력한 수를 한 변으로 하는 정사각형을 (*)기호로 출력하는 프로그램*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j;
+//	int n;
+//	printf("사각형을 출력합니다.");
+//	printf("입력할 수 : "); scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++) {
+//		for (j = 1; j <= n; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
