a25d2cba09e81ad139211e00b17f109870456947
zsueue <zsueue@gmail.com>
2021-01-04 22:33:39 +0900

    Create 1-6.cpp

---
+++ 1-6.cpp
@@ -0,0 +1,21 @@
+///*6. 실습 1-4에서 while문이 종료될 때 변수 i 값이 n+1이 됨을 확인하세요.*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, n;
+//	int sum;
+//	printf("1부터 n까지의 값을 구하시오.\n");
+//	printf("n의 값 : "); scanf_s("%d", &n);
+//	sum = 0;
+//	i = 1;
+//	while (i <= n) {
+//		sum += 1;
+//		i++;
+//	}
+//	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+//	printf("변수 i의 값은 %d입니다.\n", i);
+//
+//	return 0;
+//}
