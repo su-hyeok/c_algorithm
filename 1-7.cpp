f8c9808581f08d779282afc9f284dfec46c730c5
zsueue <zsueue@gmail.com>
2021-01-04 22:33:42 +0900

    Create 1-7.cpp

---
+++ 1-7.cpp
@@ -0,0 +1,20 @@
+///*7. n이 3이면 1 + 2 + 3 = 6으로 출력하는 프로그램*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, n;
+//	int sum;
+//	printf("1부터 n까지의 값을 구하시오.\n");
+//	printf("n의 값 : "); scanf_s("%d", &n);
+//	sum = 1;
+//	printf("%d ", 1);
+//	for (i = 2; i <= n; i++) {
+//		sum += i;
+//		printf("+ %d ", i); 
+//	}
+//	printf("= %d\n", sum);
+//
+//	return 0;
+//}
