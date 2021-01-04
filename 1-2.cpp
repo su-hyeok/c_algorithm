3ab436bb2e7018bb17d7565604e357e81edcea58
zsueue <zsueue@gmail.com>
2021-01-04 22:33:30 +0900

    Create 1-2.cpp

---
+++ 1-2.cpp
@@ -0,0 +1,29 @@
+///*세 값의 최솟값을 구하는 함수 min3를 작성하세요.*/
+//
+//#include <stdio.h>
+//
+//int min3(int a, int b, int c)
+//{
+//	int min = a;
+//	if (b < min) {
+//		min = b;
+//	}
+//	if (c < min) {
+//		min = c;
+//	}
+//
+//	return min;
+//}
+//
+//int main(void)
+//{
+//	int a, b, c;
+//	int min;
+//	printf("세 정수의 최솟값을 구하시오.\n");
+//	printf("a의 값 : "); scanf_s("%d", &a);
+//	printf("b의 값 : "); scanf_s("%d", &b);
+//	printf("c의 값 : "); scanf_s("%d", &c);
+//	printf("최솟값은 %d입니다.\n", min3(a, b, c));
+//
+//	return 0;
+//}
