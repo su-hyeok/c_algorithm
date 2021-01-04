7bd073c79d1cde6e14552cd71a17b9848446f7c5
zsueue <zsueue@gmail.com>
2021-01-04 22:34:06 +0900

    Create 1-15.cpp

---
+++ 1-15.cpp
@@ -0,0 +1,20 @@
+///*15. 입력한 높이와 너비에 맞는 직사각형을 (*)기호로 출력하는 프로그램*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j;
+//	int row, col;
+//	printf("직사각형을 출력합니다.\n");
+//	printf("높이 : "); scanf_s("%d", &col);
+//	printf("너비 : "); scanf_s("%d", &row);
+//	for (i = 1; i <= col; i++) {
+//		for (j = 1; j <= row; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
