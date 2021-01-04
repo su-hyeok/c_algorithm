d4ffcf69220454d04c06a55ed97eb16776d6b055
zsueue <zsueue@gmail.com>
2021-01-04 22:33:45 +0900

    Create 1-8.cpp

---
+++ 1-8.cpp
@@ -0,0 +1,15 @@
+///*8. 가우스의 덧셈을 이용하여 1부터 n까지의 정수 합을 구하는 프로그램*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, n;
+//	int sum;
+//	printf("가우스의 덧셈을 이용하여 1부터 n까지의 정수 합을 구하시오.\n");
+//	printf("n의 값 : "); scanf_s("%d", &n);
+//	sum = ((1 + n) * n) / 2;
+//	printf("1부터 %d까지의 정수 합은 %d입니다.\n", n,sum);
+//
+//	return 0;
+//}
