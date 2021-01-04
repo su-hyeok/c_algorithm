7204d4f951be9a89c5d6b0aa0088047eee8694cb
zsueue <zsueue@gmail.com>
2021-01-04 22:33:53 +0900

    Create 1-11.cpp

---
+++ 1-11.cpp
@@ -0,0 +1,18 @@
+///*11. 양의 정수를 입력하고 자릿수를 출력하는 프로그램*/
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i;
+//	int num;
+//	printf("양의 정수 : "); scanf_s("%d", &num);
+//	i = 0;
+//	while (num > 0) {
+//		num = num / 10;
+//		i++;
+//	}
+//	printf("그 수는 %d자리입니다.\n", i);
+//
+//	return 0;
+//}
