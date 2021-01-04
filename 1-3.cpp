5a600f7858abf5a16b16c07623b0d1a0773de2e2
zsueue <zsueue@gmail.com>
2021-01-04 22:33:33 +0900

    Create 1-3.cpp

---
+++ 1-3.cpp
@@ -0,0 +1,33 @@
+///*네 값의 최솟값을 구하는 함수 min4를 작성하세요.*/
+//
+//#include <stdio.h>
+//
+//int min4(int a, int b, int c, int d)
+//{
+//	int min = a;
+//	if (b < min) {
+//		min = b;
+//	}
+//	if (c < min) {
+//		min = c;
+//	}
+//	if (d < min) {
+//		min = d;
+//	}
+//
+//	return min;
+//}
+//
+//int main(void)
+//{
+//	int a, b, c, d;
+//	int min;
+//	printf("네 정수의 최솟값을 구하시오.\n");
+//	printf("a의 값 : "); scanf_s("%d", &a);
+//	printf("b의 값 : "); scanf_s("%d", &b);
+//	printf("c의 값 : "); scanf_s("%d", &c);
+//	printf("d의 값 : "); scanf_s("%d", &d);
+//	printf("최솟값은 %d입니다.\n", min4(a, b, c, d));
+//
+//	return 0;
+//}
